adf3a68a5644dc4ee5ec8ed7b7df15df540f5b15
unknown <mohammadshaiq34@yahoo.com>
2018-07-16 17:53:41 +0500

    modified olx-pak

--- js/viewAd.js
+++ js/viewAd.js
@@ -8,7 +8,39 @@ let adToView = localStorage.getItem('adToView');
 let catsToView = localStorage.getItem("catsToView");
 console.log(adToView,catsToView)
 
-firestore.collection(catsToView).doc(adToView)
-    .get().then(doc => {
-        console.log(doc.data())
+let data;
+
+firestore.collection(catsToView).doc(adToView).get()
+    .then(function (snapshot) {
+        console.log(snapshot.data());
+        data = snapshot.data();
     })
+
+
+let currentSlide = 0;
+let slider = document.getElementById("slider");
+
+function next() {
+    if (currentSlide == (data.imgs.length - 1)) {
+
+    } else {
+        slider.backgroundImage = `url(${data.imgs[currentSlide]})`;
+        currentSlide++;
+    }
+}
+
+function back() {
+    if(currentSlide == 0){
+
+    }else{
+        slider.backgroundImage = `url(${data.imgs[currentSlide]})`
+        currentSlide--
+    }
+
+}
+
+
+
+
+
+
--- js/viewCat.js
+++ js/viewCat.js
@@ -3,14 +3,49 @@ const settings = {/* your settings... */ timestampsInSnapshots: true };
 firestore.settings(settings);
 
 
+console.log(firestore)
+
 
 const cat = localStorage.getItem("catsToView");
 
 
 
+let outcome = document.getElementById("output");
+
+outcome.innerHTML = "";
+
 firestore.collection(cat).get()
-    .then((snapshot) => {
-        snapshot.forEach((doc) => {
-            console.log(doc.data())
-        })
+    .then(function (doc) {
+        let indexNo = 0;
+        doc.forEach(selectFile => {
+            console.log(doc.docs)
+            console.log(selectFile.data());
+            localStorage.setItem("adderId",selectFile.data().adderId)
+            let data = selectFile.data()
+            outcome.innerHTML += `
+            <div class="container result mt-2 d-flex flex-row" id="${doc.docs[indexNo].id}" onclick="viewAd(event)">
+            <div class="adImg d-flex">
+                <img src="${data.imgs}" class="w-80 align-self-center m-auto">
+            </div>
+            <div class="ml-4 justify-content-md-start justify-content-sm-center w-100">
+                <h5 id="title" class="mb-0">${data.Title}</h5>
+                <p class="text-muted mb-0">${data.Catagory}</p>
+                <h5 class= "float-left price" >Rs.${data.price}</h5>
+                <br/>
+                <p class="text-muted mb-0">${data.Description}</p>
+            </div>
+            </div>
+            ` 
+            indexNo++;
+        });
     })
+
+    
+
+
+
+    function viewAd(event){
+        let target = event.target.parentNode.parentNode;
+        localStorage.setItem("adToView",target.id);
+        window.location = "viewAd.html"
+    }
